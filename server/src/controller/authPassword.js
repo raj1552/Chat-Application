88a4afc54eaac780abc009ed8d6d2a5af216afef
Anuraj Shrestha <anurajshrestha75@gmail.com>
2024-02-19 00:40:56 -0500

    made forgetpasssword and resetpassword

--- server/src/controller/authPassword.js
+++ server/src/controller/authPassword.js
@@ -1,5 +1,6 @@
 import pool from '../../db/config.js'
 import nodemailer from 'nodemailer'
+import bcrypt from 'bcrypt'
 
 const forgetPassword = async (req, res) => {
     try {
@@ -12,27 +13,31 @@ const forgetPassword = async (req, res) => {
         if (rows.length === 0) {
             return res.status(400).json("User does not exist");
         }
+        
+        const userId = rows[0].id
+        const otp= Math.floor(1000 + Math.random() * 9000);
+        const otpExpires = new Date();
+        otpExpires.setMinutes(otpExpires.getMinutes() + 10)
+        
+        await pool.query('INSERT INTO otp (user_id, otp, expiration_time) values($1, $2, $3);', [userId, otp, otpExpires])
 
-        const opt = Math.floor(1000 + Math.random() * 9000);
-        const optExpires = new Date();
-        optExpires.setMinutes(optExpires.getMinutes() + 1);
-
-        var smtpConfig = {
+        const smtpConfig = {
           host: 'smtp.gmail.com',
           port: 465,
-          secure: true, // use SSL
+          secure: true, 
           auth: {
               user: 'anurajshrestha75@gmail.com',
               pass: 'yvms uizw dher pxta '
           }
       };
-      var transporter = nodemailer.createTransport(smtpConfig);
+
+      const transporter = nodemailer.createTransport(smtpConfig);
 
         const mailOptions = {
             from: 'anurajshrestha75@gmail.com',
             to: email,
             subject: 'Reset Password',
-            text: `Your Password reset code is ${opt}`
+            text: `Your Password reset code is ${otp}`
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
@@ -51,7 +56,28 @@ const forgetPassword = async (req, res) => {
 
 const resetPassword = async (req, res) =>{
     try{
+    const { otp, password, confirmpassword } = req.body;
 
+    if(!password || !confirmpassword || !otp){
+      return res.status(400).json("Password ,Confirmpassword and OTP required!")
+    }
+    
+    const { rows } = await pool.query('SELECT * FROM otp where otp = $1 and expiration_time > now();', [otp]);
+    
+    if(rows.length == 0){
+      return res.status(400).json("Invalid or expired otp")
+    }
+
+    if(password != confirmpassword){
+      return res.status(400).json("Password & Confirmpassword do not match")
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await pool.query("UPDATE users SET password = $1, confirmpassword = $2 where id = $3;", [hashedPassword, hashedPassword, rows[0].userId])
+
+    await pool.query('DELETE FROM otp;')
+    res.status(200).json({message : 'Password reset sucessfully', password:hashedPassword})
     }
     catch(error){
         console.error(error)
